059f160a1a220b231d77a4086daa87535f6a4976
Williams-Bournissent <Williams.bournissent@gmail.com>
2021-12-20 18:19:18 -0300

    correcciones

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express('');
 const path = require('path');
 
-const staticFiles = express.static(path.join(__dirname, '/Public'));
+const staticFiles = express.static(path.join(__dirname, '/public'));
 app.use(staticFiles);
 
 app.listen(process.env.PORT || 3000, (req, res) => {
